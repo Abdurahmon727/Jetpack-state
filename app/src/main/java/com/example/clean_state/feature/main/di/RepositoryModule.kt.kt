c80eaae250be62a6f60835f9657e7ac65b1a1ea9
Abdurahmon727 <abdurahmon727.uz@gmail.com>
2023-09-08 15:00:35 +0500

    intial commit

--- app/src/main/java/com/example/clean_state/feature/main/di/RepositoryModule.kt.kt
+++ app/src/main/java/com/example/clean_state/feature/main/di/RepositoryModule.kt.kt
@@ -1,21 +1,18 @@
-//package com.example.clean_state.feature.main.di.com.example.clean_state.feature.main.di
-//
-//import MainPageRepositoryImpl
-//import com.example.clean_state.feature.main.data.repository.MainPageRepository
-//import dagger.Binds
-//import dagger.Module
-//import dagger.Provides
-//import dagger.hilt.InstallIn
-//import dagger.hilt.components.SingletonComponent
-//import javax.inject.Singleton
-//
-//
-//@Module
-//@InstallIn(SingletonComponent::class)
-//abstract  class RepositoryModule {
-////    @Provides
-////    @Singleton
-////    abstract fun bindMainPageRepository(): MainPageRepository{
-////        return
-////    }
-//}
+package com.example.clean_state.feature.main.di.com.example.clean_state.feature.main.di
+
+import MainPageRepositoryImpl
+import com.example.clean_state.feature.main.data.repository.MainPageRepository
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface  RepositoryModule {
+//    @Binds
+//     fun bindMainPageRepository(impl: MainPageRepositoryImpl): MainPageRepository
+}
